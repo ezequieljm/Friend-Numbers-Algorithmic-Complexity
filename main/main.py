aeaf7746d9544cbc7160cdab089ca23c516435d6
ezequieljm <ezequieljm020@gmail.com>
2022-04-07 16:09:42 -0300

    primes factorization

--- main/main.py
+++ main/main.py
@@ -3,9 +3,14 @@ from test_modules import test_of_module
 
 # The main function entry point
 def main(*args, **kwargs):
-    #print(f"""Dividers of {args[0]} are:\n{test_of_module.dividers_of(args[0])}""")
-    print(f"""Sum of the divisors of {args[0]} = {test_of_module.sum_dividers_of(args[0])}""")
+    #test_of_module.divs_range(50000)
+    primes = test_of_module.get_lazy_prime()
+    test_of_module.primes_ultil(50000, primes)
+
+
+
+
 
 # Control of module
 if __name__ == "__main__":
-    main(int(argv[1]))
+    main()
